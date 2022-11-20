35e8a18d2993242078c6fd3bfd377a8307a80dbb
unknown <frp.plata@gmail.com>
2022-11-20 14:15:23 -0500

    add styled components and create the nav header

--- webtravelApp/my-trip/src/components/navMenu/index.tsx
+++ webtravelApp/my-trip/src/components/navMenu/index.tsx
@@ -1,47 +1,76 @@
 import { Fragment, useState } from "react";
 import { Outlet, Link } from "react-router-dom";
+import styled from "styled-components";
+import flag from "../../images/flag.jpg";
 
 const NavMenu = () => {
-  const [sticky, setSticky] = useState(false)
+  const [sticky, setSticky] = useState(false);
 
-  const navBarSticky = ()=>{
-    if(window.scrollY >= 232){
-      setSticky(true)
-    }else{
-      setSticky(false)
+  const navBarSticky = () => {
+    if (window.scrollY >= 232) {
+      setSticky(true);
+    } else {
+      setSticky(false);
     }
-  }
+  };
+  console.log(window.scrollY, "que vaina ala");
 
-  window.addEventListener('scroll', navBarSticky)
+  window.addEventListener("scroll", navBarSticky);
+  const List = styled.ul`
+    display: flex;
+    justify-content: space-evenly;
+    list-style-type: none;
+    a {
+      text-decoration: none;
+      color: gold;
+    }
+    ${({ ...sticky }) => !sticky && "position: fixed"}
+  `;
+
+  const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 150px;
+    width: 100%;
+
+    img {
+      width: 100%;
+      height: 150px;
+      position: relative;
+      bottom: 160px;
+      z-index: -1;
+    }
+  `;
+  const TitleNav = styled.h1`
+    text-align: center;
+    color: darkblue;
+  `;
   return (
-    <Fragment>
-   
-      <nav className={sticky ? 'dos activeNav': 'dos'}>
-        <div className="wrap">
+    <>
+      <Container>
+
+          <TitleNav>Toro Travel and Entertaiment</TitleNav>
           <nav className="tres">
-            <ul>
+            <List>
               <li>
                 <Link to="/guatape">Guatape</Link>
               </li>
               <li>
-                  <Link to="/graffity-tour">Garffity Tour</Link>
-              
+                <Link to="/graffity-tour">Garffity Tour</Link>
               </li>
               <li>
-                 <Link to="/san-felix">San Felix</Link> 
-               
+                <Link to="/san-felix">San Felix</Link>
               </li>
               <li>
-                  <Link to="/sabaneta">Sabaneta</Link> 
+                <Link to="/sabaneta">Sabaneta</Link>
               </li>
-              
-            </ul>
+            </List>
           </nav>
-        </div>
-      </nav>
+     
+        <img src={flag} alt="" />
+      </Container>
       <Outlet />
-   
-    </Fragment>
+    </>
   );
 };
 
